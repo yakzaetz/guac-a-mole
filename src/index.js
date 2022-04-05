f4feaf65c3d7adf0f1f69b3717209155ea9405c1
yakzaetz <yakzaetz@gmail.com>
2022-04-05 15:13:54 -0400

    first presentation

--- src/index.js
+++ src/index.js
@@ -4,23 +4,26 @@ import Game from "./scripts/game"
 
 document.addEventListener('DOMContentLoaded', ()=>{
 
-    // const open = document.getElementById("open");
-    // const modal_container = document.getElementById("modal-container");
-    // const close = document.getElementById("close")
+    const open = document.getElementById("open");
+    const modal_container = document.getElementById("modal-container");
+    const close = document.getElementById("close")
 
-    // open.addEventListener('click', () => {
-    //     modal_container.classList.add("show");
-    // });
+    open.addEventListener('click', () => {
+        modal_container.classList.add("show");
+    });
 
-    // close.addEventListener("click", () => {
+    close.addEventListener("click", () => {
+        modal_container.classList.remove("show")
+    })
+
+    // modal_container.addEventListener("click", () => {
     //     modal_container.classList.remove("show")
     // })
+
     let el = document.querySelector(".gam")
     let view = new View(game, el);
     view.setupBoard();
-    let game = new Game();
-    // let ul = 
-
+    let game = new Game(); 
 
     window.game = game
 })
